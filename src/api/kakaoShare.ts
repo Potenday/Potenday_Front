6678cd8e8841419af55b8402fcc5bf55b97dbeef
daheejo <happyj6528@gmail.com>
2023-04-01 19:08:25 +0900

    feat: kakao share

---
+++ src/api/kakaoShare.ts
@@ -0,0 +1,31 @@
+import { BASE_URL } from "./constant";
+// type KakaoShareResult = "success" | "fail";
+type Props = {
+    // route?: string | undefined;
+    // title?: string | undefined;
+    image?: string | undefined;
+}
+export const kakaoShare = ({image}:Props) => {
+      window.Kakao.Share.sendDefault({
+        objectType: "feed",
+        content: {
+          title: '함께 키우는 이야기숲',
+          description: "나랑 비슷하다고 생각하는 나무를 심어주세요!", 
+          imageUrl: image,
+          link: {
+            mobileWebUrl: BASE_URL,
+            webUrl: BASE_URL
+          }
+        },
+        buttons: [
+          {
+            title: "나무 심으러 가기",
+            link: {
+              mobileWebUrl: BASE_URL,
+              webUrl: BASE_URL
+            }
+          }
+        ]
+      });
+    }
+  
